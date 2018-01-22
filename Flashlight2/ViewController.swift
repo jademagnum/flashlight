43012cb00d490b471f33d8e15e48792a4bcfdb7e
jademagnum <jademagnum@gmail.com>
2018-01-22 14:48:59 -0700

    Initial Commit

---
+++ Flashlight2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashlight2
+//
+//  Created by Jade Thomason on 1/22/18.
+//  Copyright © 2018 Jade Thomason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
